5cdec6a343d96eef27b9bc99cd2e506562599ff4
JenniferCarmen <jennifercarmens@gmail.com>
2018-05-10 12:55:04 -0500

    sass-html gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,8 @@ var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var minifyhtml = require('gulp-minify-html');
 
-var config = {
-  source: './src/',
-  dist: './dist/'
-};
 
-var paths = {
-  assets: "assets/",
-  html: "*.html",
-  sass: "scss/*.scss",
-  css: "css/*.css"
-};
 
-var sources = {
-  assets: config.source + paths.assets,
-  html: config.source + paths.html,
-  sass: paths.source + paths.sass,
-  css: config.source + paths.css,
-};
-
-gulp.task('html', ()=> {
-  gulp.src(sources.html)
-  .pipe(gulp.dest(config.dist));
-});
 gulp.task('html', ()=> {
   return gulp.src('src/*.html')
    .pipe(concat('index.html'))
@@ -43,7 +22,3 @@ gulp.task('css', function(){
 })
 
 
-gulp.task('watch', function () {
-  gulp.watch(sources.html, ['html']);
-  gulp.watch([sources.css, sources.sass], ['css']);
-});
